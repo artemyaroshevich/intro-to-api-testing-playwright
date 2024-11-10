5bee9b6dd8387fd2dcef17f24d98a2dca1eee35a
artemyaroshevich <artem.yaroshevich.2205@gmail.com>
2024-11-10 11:21:26 +0300

    hw 11 check jwt token and incorrect http method

--- tests/login-api.test.ts
+++ tests/login-api.test.ts
@@ -1,6 +1,7 @@
 
 import { expect, test } from '@playwright/test'
 import { LoginDto } from './dto/LoginDto'
+import { StatusCodes } from 'http-status-codes'
 const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 
@@ -8,18 +9,28 @@ test.describe('Tallinn delivery API tests', () => {
   test('login with correct data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
     const response = await request.post(`${serviceURL}${loginPath}`, {
-      data: requestBody
+      data: requestBody,
     })
-    // const responseBody = await response.text()
-    expect(response.status()).toBe(200)
+    expect(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.text()
+    const jwt = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+    expect(responseBody).toMatch(jwt)
   })
+
 
   test('login with incorrect data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
     const response = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody
     })
-    // const responseBody = await response.text()
-    expect(response.status()).toBe(401)
+    expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+  })
+
+  test('login with incorrect http method', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithIncorrectData()
+    const response = await request.get(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    })
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 })
